a0db9225bf0900b32774456965f00a15fbc2d2d3
kdemoor <kdemoor@PPC18634>
2023-03-28 14:54:56 +0200

    Add project files.

---
+++ kdemoor_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("kdemoor_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("kdemoor_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("2E344611-FF19-48EB-841A-F5EBF59D094A")]
+[assembly: AssemblyVersion("1.0.0.0")]
